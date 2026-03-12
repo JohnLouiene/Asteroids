eb5bc980607f3823bce9b239517904e5cb9c2296
JohnLouiene <johnlouiene@gmail.com>
2026-03-12 22:02:49 +0800

    Added player entity and rotation mechanics

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 def main():
     pygame.init()
+    __clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     version = pygame.version.ver
     game_started = True
+
+    #Player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while game_started == True:
         log_state()
@@ -14,12 +22,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        #Player rendering
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 
-    print(f"Starting Asteroids with pygame version: {version}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = __clock.tick(60) / 1000
+        print(dt)
+        
 
+        
 
 if __name__ == "__main__":
     main()
