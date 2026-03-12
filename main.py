10124d530cb10f2170f4b83a4f5ca65a2ff12061
JohnLouiene <johnlouiene@gmail.com>
2026-03-12 12:59:19 +0800

    Initializing current project files

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    version = pygame.version.ver
+    print(f"Starting Asteroids with pygame version: {version}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
